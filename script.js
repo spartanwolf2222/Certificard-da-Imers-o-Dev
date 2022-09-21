e8476e23518a61face705f1bf1955a7abce0711b
Diego <113812912+spartanwolf2222@users.noreply.github.com>
2022-09-21 20:38:52 -0300

    Imersão Dev

---
+++ script.js
@@ -0,0 +1,8 @@
+//para estilizar uma classe no CSS, deve se usar um . e o nome da classe
+//border-radius vai mexer com a borda, deixando mais arredondado
+//box-shadow vai fazer com que faça uma sombra na qual da a impressão de que está santando para fora
+//margin é do elemento externo para o elemento que estamos estilizando e o padding é o contrário
+
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
